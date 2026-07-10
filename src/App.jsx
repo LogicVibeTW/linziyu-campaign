95615389d70c9b969970fe8141d0f80e404a6f82
ericandy0731-maker <ericandy0731@gmail.com>
2026-07-10 17:26:54 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -32,6 +33,7 @@ export default function App() {
 
       <Footer />
       <FloatingActions />
+      <Analytics />
     </>
   );
 }
